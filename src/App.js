dfa3a27ca476ee00e4b910f685d4fbbecc3228e4
Kieuthai1 <kieuthai093@gmail.com>
2024-07-27 18:51:28 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-
+import Header from './components/Header/Header' ;
 
 const App = () => {
 
@@ -7,18 +7,7 @@ const App = () => {
   return (
   
     <div className="app-container">
-
-      hello xin chao cac ban 
-      <button class="btn btn-primary">test</button>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header> */}
+      <Header/>
     </div>
   );
 }
