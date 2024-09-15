b42746ed749b2f87e75a15eb2cdf7f33d0de98f4
Kieuthai1 <kieuthai093@gmail.com>
2024-09-16 00:00:40 +0700

    design q/a

--- src/components/Admin/Content/Quiz/MangageQuiz copy.js
+++ src/components/Admin/Content/Quiz/MangageQuiz copy.js
@@ -1,13 +1,11 @@
 import { useState } from 'react';
-import Select from 'react-select';
 import './ManageQuiz.scss';
+import Select from 'react-select';
 import { postCreatNewQuiz } from '../../../../services/apiService';
 import {  toast } from 'react-toastify';
 import TableQuiz from './TableQuiz';
 import Accordion from 'react-bootstrap/Accordion';
 import { getAllQuizForAdmin } from "../../../../services/apiService";
-import QuizQA from './QuizQA';
-import AssignQuiz from './AssignQuiz';
 
 
 const options = [
@@ -120,13 +118,13 @@ const ManageQuiz = (props) =>{
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>Update Q/A Quizzes</Accordion.Header>
                     <Accordion.Body>
-                  <QuizQA />
+                  32131232
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
                     <Accordion.Header>Assign to Users</Accordion.Header>
                     <Accordion.Body>
-                    <AssignQuiz /> 
+                  32131232
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
